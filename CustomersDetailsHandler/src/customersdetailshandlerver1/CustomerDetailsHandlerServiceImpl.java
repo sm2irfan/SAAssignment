c3c86db07cead6f2bf3beed7736feb1c669d458d
Sidpavn <68677837+Sidpavn@users.noreply.github.com>
2021-04-18 22:03:09 +0530

    Update CustomerDetailsHandlerServiceImpl.java

--- CustomersDetailsHandler/src/customersdetailshandlerver1/CustomerDetailsHandlerServiceImpl.java
+++ CustomersDetailsHandler/src/customersdetailshandlerver1/CustomerDetailsHandlerServiceImpl.java
@@ -13,39 +13,63 @@ public class CustomerDetailsHandlerServiceImpl implements CustomerDetailsHandler
 		}
 	}
 	
-	
-	
-	
-	
-	
-	String[] keys = new String[] {"id1011", "id1012", "id1013"};
-	String[] values = new String[] {"aaa", "bbbb", "cccc"};
-	
+	String[] keys = new String[] {"id1011", "id1012", "id1013","id1014","id1015"};
+	String[] values1 = new String[] {"Kamal", "Ravi", "Nimal","Kumar","Raj"};
+	String[] values2 = new String[] {"Jaffna", "Mannaar", "Colombo","Kandy","Colombo"};
+	String[] values3 = new String[] {"0771234567", "0772547638", "0772586935","0778957241","0771452689"};
+	String[] values4 = new String[] {"951425869V", "961478256V", "920415286V","992587456V","982145687V"};
+	 
 	
 	
 	@Override
 	public String name(String customerID) {
 		// TODO Auto-generated method stub
-		Map<String, String> mapName = new HashMap<String, String>();
-		loadIntoHash(keys, values, mapName);
+	        Map<String, String> mapName = new HashMap<String, String>();
+		loadIntoHash(keys, values1, mapName);
 		String name = mapName.get(customerID);
-		if (name == null) {
-			return "please enter correct customer ID";
-		}else {
+		
 			return name;
-		}
+		
 	}
 	
+	@Override
+	public String address(String customerID) {
+		// TODO Auto-generated method stub
+		Map<String, String> mapaddress = new HashMap<String, String>();
+		loadIntoHash(keys,values2,mapaddress);
+		String address = mapaddress.get(customerID);
+		
+			return address;
+		
+		
+	}
+
 	
 	
 	
+	@Override
+	public String phoneNo(String customerID) {
+		// TODO Auto-generated method stub
+	        Map<String, String> mapphoneNo = new HashMap<String, String>();
+		loadIntoHash(keys, values3, mapphoneNo);
+		String phoneNo = mapphoneNo.get(customerID);
+		
+			return phoneNo;
+		
+	}
 	
 	
 
 	@Override
-	public String address(String customerID) {
+	public String NIC(String customerID) {
 		// TODO Auto-generated method stub
-		return "kalmunai";
+	        Map<String, String> mapNIC = new HashMap<String, String>();
+		loadIntoHash(keys, values4, mapNIC);
+		String NIC = mapNIC.get(customerID);
+		
+			return NIC;
+		
 	}
-
-}
+	
+}	
+	
