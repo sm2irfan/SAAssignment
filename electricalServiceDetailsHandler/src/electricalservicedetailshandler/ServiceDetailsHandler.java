a8fbad9df6e48e23a69c554ba083722bf8a4e326
Kajathees Premendran <60744394+kajan03@users.noreply.github.com>
2021-04-18 19:50:46 +0530

    Update ServiceDetailsHandler.java

--- electricalServiceDetailsHandler/src/electricalservicedetailshandler/ServiceDetailsHandler.java
+++ electricalServiceDetailsHandler/src/electricalservicedetailshandler/ServiceDetailsHandler.java
@@ -1,9 +1,17 @@
 package electricalservicedetailshandler;
 
+import java.util.List;
+
+
+
 public interface ServiceDetailsHandler {
 	
 	
 	public String PhaseDetails(String customerID);
 	public String TransformerRating(String customerID);
 	public String customerType(String customerID);
+	public int addElectricService(String CustomerId,String CustomerType,String Phase,String TransformerRating);
+	public int removeElectricService(String CustomerId);
+	public   List<Service> listService();//Returns the Service list
+
 }
