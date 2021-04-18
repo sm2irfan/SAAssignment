e765b905913ca81a9dd4a303a97aedc137c56848
Danuzon <68749632+Danuzon@users.noreply.github.com>
2021-04-18 21:41:04 +0530

    Update CustomerDetailsHandlerService.java

--- CustomersDetailsHandler/src/customersdetailshandlerver1/CustomerDetailsHandlerService.java
+++ CustomersDetailsHandler/src/customersdetailshandlerver1/CustomerDetailsHandlerService.java
@@ -5,4 +5,6 @@ public interface CustomerDetailsHandlerService {
 
 	public String name(String customerID);
 	public String address(String customerID); 
+	public String phoneNo(String customerID);
+	public String NIC(String customerID); 
 }
